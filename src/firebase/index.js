1d41f2fc919dead23ed25134bf04f5261b153dae
unknown <342496775@TDSB.ON.CA>
2024-12-10 11:30:45 -0500

    initiated firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBTMj3TzvuFPWa61NBqmkAZEIMMxdlgarI",
+    authDomain: "summative-d545d.firebaseapp.com",
+    projectId: "summative-d545d",
+    storageBucket: "summative-d545d.firebasestorage.app",
+    messagingSenderId: "455955456486",
+    appId: "1:455955456486:web:554899447c0cb1fc1bf627"
+};
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
